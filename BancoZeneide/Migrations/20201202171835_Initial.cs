5f2d4d259cd909186219389665e69e233c5d9534
lucas.galvao@aarim.com <lucas.galvao@aarim.com>
2020-12-02 14:22:31 -0300

    Criação da primeira migration + salvamento no banco

---
+++ BancoZeneide/Migrations/20201202171835_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BancoZeneide.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Cep = table.Column<string>(nullable: true),
+                    Endereco = table.Column<string>(nullable: true),
+                    Numero = table.Column<int>(nullable: false),
+                    Cidade = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Cnpj = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
