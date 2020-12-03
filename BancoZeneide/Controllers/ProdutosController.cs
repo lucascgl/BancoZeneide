f4bbc36f332800ed9efa45baccd01f2b1c9c0701
lucas.galvao@aarim.com <lucas.galvao@aarim.com>
2020-12-03 15:07:00 -0300

    Criacao de novos controllers e views

---
+++ BancoZeneide/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoZeneide.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
